1c91cd81820bdeb65489415e88366c0623b593f9
Marie Assine Ghantous <marie.g@sirenanalytics.com>
2023-10-23 12:41:08 +0300

    'CLEANING

--- detect.py
+++ detect.py
@@ -2,10 +2,8 @@ from bytetrack_inference_demo_yolov5_with_speed import Detection
 from processing import generate_frames
 import torch
 
-WEIGHTS_PATH = "../best.pt"
-SOURCE_VIDEO_PATH = "../clips/08fd33_4.mp4"
-frame_iterator = iter(generate_frames(video_file=SOURCE_VIDEO_PATH))
-frame = next(frame_iterator)
-model = torch.hub.load('ultralytics/yolov5', 'custom', WEIGHTS_PATH, device=0)
-
+def loadyolo(WEIGHTS_PATH,SOURCE_VIDEO_PATH):
+    frame_iterator = iter(generate_frames(video_file=SOURCE_VIDEO_PATH))
+    model = torch.hub.load('ultralytics/yolov5', 'custom', WEIGHTS_PATH, device=0)
+    return model, frame_iterator
 
